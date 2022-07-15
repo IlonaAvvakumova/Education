bdb5eb705f778477549b8b0648a8c7aa5d946e0a
Ilona <avvakumova_ilona@mail.ru>
2022-07-15 12:12:09 +0300

    Module 1.2. before review

--- src/src/main/java/com/IlonaA/javacore/rightGames/GameLogic.java
+++ src/src/main/java/com/IlonaA/javacore/rightGames/GameLogic.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 
 public class GameLogic {
     private final Scanner scanner = new Scanner(System.in); //делаем сканер константой
-    private final Field field = new Field(); //создаем прострастранство игры
+    private final Field field = new Field();
 
     public void start() {
-        field.setFigure1(getPlayer1Input());
+        field.setFigure1(getPlayer1Input()); //сетер устанавливает значение выбора вызывая метод ввода с консоли для каждого игрока
         field.setFigure2(getPlayer2Input());
-        String result = checkWinner();
+        String result = checkWinner();// вызывается метод для выявления победителя для объекта стринг, так как он не статик
         System.out.println(result);
     }
 
-    private Figure getPlayer1Input() {
+    private Figure getPlayer1Input() { //ввод значения для первого игрока
         System.out.println("Пожалуйста введите число: " + "\n" + "1-камень ✊" + "\n" + "2-ножницы ✌" + "\n" + "3-бумага ✋");
         System.out.println("Выбор первого игрока ");
         int choice1 = scanner.nextInt();
-        while (choice1 < 1 || choice1 > 3) {
+        while (choice1 < 1 || choice1 > 3) {//проверка на не корректный ввоод
             System.out.println("Enter correct input: ");
             choice1 = scanner.nextInt();
         }
         return Figure.getByValue(choice1);
     }
 
-    private Figure getPlayer2Input() {
+    private Figure getPlayer2Input() {//ввод значения для второго игрока
         System.out.println("Выбор второго игрока ");
         int choice2 = scanner.nextInt();
         while (choice2 < 1 || choice2 > 3) {
@@ -34,8 +34,9 @@ public class GameLogic {
         return Figure.getByValue(choice2);
     }
 
-    private String checkWinner() {
-        if (field.getFigure1().equals(field.getFigure2()))
+    private String checkWinner() {//определение победителя
+       // if (field.getFigure1().equals(field.getFigure2()))
+        if (field.getFigure1()==field.getFigure2())
             return "Ничья, ваш противник выбрал такой же элемент";
         else if (field.getFigure1() == (Figure.STONE) && field.getFigure2() == (Figure.SCISSORS) || field.getFigure1() == (Figure.SCISSORS) && field.getFigure2() == (Figure.PAPER) ||
         field.getFigure1() == (Figure.PAPER) && field.getFigure2() == (Figure.STONE))
--- src/src/main/java/com/IlonaA/javacore/rightGames/GameRunner.java
+++ src/src/main/java/com/IlonaA/javacore/rightGames/GameRunner.java
@@ -2,7 +2,7 @@ package src.main.java.com.IlonaA.javacore.rightGames;
 
 public class GameRunner {
     public static void main(String[] args) {
-        GameLogic gameLogic = new GameLogic();
+        GameLogic gameLogic = new GameLogic();//создаем прострастранство игры
         gameLogic.start();
     }
 }
